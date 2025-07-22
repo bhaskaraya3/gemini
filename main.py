d6970b90be9c8a608f028c4c35a3b84e14ef69a2
Bhaskar Arya <bhashkararya2003@gmail.com>
2025-07-22 19:36:03 +0530

    adding

---
+++ main.py
@@ -0,0 +1,18 @@
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyAzCPZTyrQuziZHoFw5T063F_YEGT9avCg")
+
+model = genai.GenerativeModel("models/gemini-1.5-flash")
+
+chat = model.start_chat()
+
+print("Chat with Gemini! Type 'exit' to quit.")
+while True:
+    user_input = input("You : ")
+    if(user_input.lower()=="exit"):
+        break
+    response = chat.send_message(user_input)
+    print("Gemini",response.text)
+
+
+
